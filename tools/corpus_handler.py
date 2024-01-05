80b152b2f818c4c6a6686f09d5e11ccc536fb121
FORTE Theodore <theodore.forte@skezi.eu>
2024-01-05 16:00:25 +0100

    Method to get the sequence of sentences of a corpus

--- tools/corpus_handler.py
+++ tools/corpus_handler.py
@@ -4,9 +4,13 @@ class CorpusHandler:
 
     def save_corpus(list_sentences,output_filename):
 
-        with open(f"training_data/{output_filename}.txt","w",encoding='utf-8') as file:
+        with open(f"corpus/{output_filename}.txt","w",encoding='utf-8') as file:
             
             for sentence in list_sentences:
                 file.write(sentence+"\n")
     
-    
+    def load_corpus(filename):
+
+        with open(f"corpus/{filename}.txt","r") as file: 
+            lines = [line.strip() for line in file.readlines()]
+            return lines
