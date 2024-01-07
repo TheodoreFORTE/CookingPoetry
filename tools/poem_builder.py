1602c6343c7b94ef1d7359c8da2ca90f9013354e
FORTE Theodore <theodore.forte@skezi.eu>
2024-01-07 17:59:35 +0100

    The main class that must generates poems

---
+++ tools/poem_builder.py
@@ -0,0 +1,24 @@
+from .sonnet import Sonnet
+
+class PoemBuilder: 
+
+    '''
+    Given an a nltk model that has been fitted,  this class must be able to generate 
+    poems following theirs constraints (rhymes, shape, length of verses) for every type supported.
+        
+    
+    '''
+
+
+
+    def __init__(self,model):
+
+        self.model=model
+
+
+    
+    def fill_poem(self,poem=Sonnet(),start=None):
+        
+        #To be implemented
+        
+        pass
