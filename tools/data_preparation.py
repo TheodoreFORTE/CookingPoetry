730533630f8fa2d0c9fc119e775a13a0d9a62eaa
FORTE Theodore <theodore.forte@skezi.eu>
2024-01-07 08:22:56 +0100

    Debug + better syntax

--- tools/data_preparation.py
+++ tools/data_preparation.py
@@ -2,6 +2,7 @@ from .corpus_handler import CorpusHandler
 from .vocabulary_handler import VocabularyHandler
 from nltk.util import ngrams 
 from nltk.lm.counter import NgramCounter
+from nltk.tokenize import word_tokenize
 
 
 class DataPreparation: 
@@ -12,18 +13,24 @@ class DataPreparation:
 
     '''
 
-    def __init__(self,corpus_label,vocab_label,ngram_ids=[1,2,3]):
+    def __init__(self,corpus_label,vocab_label,ngram_ids=[1,2,3,4]):
 
         self.vocabulary=VocabularyHandler.load_vocabulary(vocab_label)
         self.corpus=CorpusHandler.load_corpus(corpus_label)
+        self.sentences=[word_tokenize(sent) for sent in self.corpus]
+
 
         self.ngram_counter=self.fill_ngrams(ngram_ids)
 
-    
-    def fill_ngrams(self,ngram_ids):
+    def fill_ngrams(self, ngram_ids):
 
-        ngram_counter=NgramCounter()
+        ngram_list = []
         for i in ngram_ids:
-            curr_ngrams=[ngrams(sent,i) for sent in self.corpus]
-            ngram_counter.update(curr_ngrams)
-        return ngram_counter
+            ngram_list+=list(tuple(ngrams(sent, i)) for sent in self.sentences)
+    
+        return NgramCounter(ngram_list)
+
+    def training_data_ngrams(sentence):
+
+        return ngrams(sentence,3)
+   
