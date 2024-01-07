08da76be572917c0b315ad1ece75968c0cd9dbe4
FORTE Theodore <theodore.forte@skezi.eu>
2024-01-07 18:00:00 +0100

    Update of the sonnet constructor method with the new poem attributes

--- tools/sonnet.py
+++ tools/sonnet.py
@@ -1,10 +1,10 @@
 from .poem import Poem
-
+from .poem_shape import PoemShape
 class Sonnet(Poem): 
 
 
     def __init__(self):
 
-        super().__init__(type="Sonnet",number_of_verses=14)
+        super().__init__(type="Sonnet",number_of_verses=14,format=PoemShape([4,4,3,3]))
         self.lengths=[12]*14
         
