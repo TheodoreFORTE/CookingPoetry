5a6130cd9ac90580b7a64a446a823f3d970f2118
FORTE Theodore <theodore.forte@skezi.eu>
2024-01-11 16:43:18 +0100

    Unbugging the constructor

--- tools/sonnet.py
+++ tools/sonnet.py
@@ -1,10 +1,11 @@
 from .poem import Poem
 from .poem_shape import PoemShape
+
 class Sonnet(Poem): 
 
 
     def __init__(self):
 
-        super().__init__(type="Sonnet",number_of_verses=14,format=PoemShape([4,4,3,3]))
+        super().__init__(genre="Sonnet",number_of_verses=14,poem_shape=PoemShape([4,4,3,3]))
         self.lengths=[12]*14
         
