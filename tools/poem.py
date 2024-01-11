5a983399e4e44bb1cc6ac0b5ec225b7486219efb
FORTE Theodore <theodore.forte@skezi.eu>
2024-01-11 16:43:02 +0100

    Method context that returns the last words given a verse_index

--- tools/poem.py
+++ tools/poem.py
@@ -1,11 +1,12 @@
 from .poem_shape import PoemShape
+from .verse import Verse
 
 class Poem: 
 
-    def __init__(self,type,number_of_verses,poem_shape):
+    def __init__(self,genre,number_of_verses,poem_shape):
 
-        self.type=type
-        self.verses=[None]*number_of_verses
+        self.genre=genre
+        self.verses=[Verse() for _ in range(number_of_verses)]
         
 
         if(isinstance(poem_shape,PoemShape)):
@@ -18,12 +19,46 @@ class Poem:
         counter=0
         output=""
        
-        for verse in verse:
+        for verse in self.verses:
 
-            if hasattr("poem_shape"):
+            if hasattr(self,"poem_shape"):
                 if counter in self.poem_shape.blank_indexes:
                     output+='\n'
-            output+=verse+'\n'
+
+            output+=" ".join(verse.content)+'\n'
             counter+=1
             
         return output
+
+    def context(self,index,number_of_words):
+
+        verse=self.verses[index]
+        if len(verse.content)>=number_of_words:
+            print("last context",verse.content[-4:])
+            return(verse.content[-4:])
+        else: 
+            if len(verse.content)>0:
+                last_words=verse.content
+            else:
+                last_words=[]
+                iterator=index-1
+                while iterator>=0 and len(last_words)<number_of_words:
+                    curr_verse=self.verses[iterator]
+                    word_count_need=number_of_words-len(last_words)
+                    if len(curr_verse.content)>word_count_need:
+                        word_to_add=curr_verse.content[-word_count_need:]
+                        for word in reversed(word_to_add):
+                            last_words.insert(0,word)
+                    else:
+                        word_to_add=curr_verse.content
+                        for word in reversed(word_to_add):
+                            last_words.insert(0,word)
+                    iterator-=1
+
+                return last_words
+
+
+        
+                
+
+
